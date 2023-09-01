7e16d7c4ef5722e3be60a2b497639c805238edd7
Nikita Stoichev <120448742+Nastoychev-1@users.noreply.github.com>
2023-09-01 16:24:15 +0300

    Added step tree

--- datasets/rides_go.py
+++ datasets/rides_go.py
@@ -1,5 +1,6 @@
 # <импорт библиотеки pandas>
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # <чтение файла с данными с сохранением в df>
 rides = pd.read_csv('https://code.s3.yandex.net/datasets/rides_go.csv')
@@ -14,3 +15,15 @@ df['duration'] = df['duration'].fillna('unknown')
 df['date'] = df['date'].fillna('unknown')
 #df.isnull().sum()- проверка пропущеных значений
 #print(df.duplicated().sum()) - проверка дубликатов
+
+#визуализация данных
+
+#расстояние, которое пользователь преодолел за одну поездку
+distance_counter = df['distance'].value_counts()
+distance_counter.plot.bar()
+#продолжительность поездок
+duration_counter = df['duration'].value_counts()
+duration_counter.plot.bar()
+
+
+plt.show() #команда активации визуализации (всавить под нужной схемой)
--- datasets/users_go.py
+++ datasets/users_go.py
@@ -1,5 +1,7 @@
 # <импорт библиотеки pandas>
+import matplotlib.pyplot as plt
 import pandas as pd
+
 # <чтение файла с данными с сохранением в df>
 user = pd.read_csv('https://code.s3.yandex.net/datasets/users_go.csv')
 df = pd.DataFrame(user)
@@ -14,3 +16,18 @@ df['subscription_type'] = df['subscription_type'].fillna('unknown')
 # <удаление всех дубликатов из таблицы df специальным методом>
 df=df.drop_duplicates().reset_index(drop=True)
 #print(df.duplicated().sum()) - проверка дубликатов
+
+#визуализация данных
+
+#частота встречаемости городов
+city_counter = df['city'].value_counts()
+city_counter.plot.bar()
+#соотношение пользователей с подпиской и без подписки
+subscription = df['subscription_type'].value_counts()
+subscription.plot.bar()
+#возраст пользователей
+age_counter = df['age'].value_counts()
+age_counter.plot.bar()
+
+plt.show() #команда активации визуализации (всавить под нужной схемой)
+
